008896088471dcbbb793c09e6400eff4b5de4a23
Aleksey <aleksey.moiseenko79@gmail.com>
2024-05-08 01:23:44 +0300

    add test cases to problem 881

--- leetcode/0881.BoatstoSavePeople/881.BoatstoSavePeople_test.go
+++ leetcode/0881.BoatstoSavePeople/881.BoatstoSavePeople_test.go
@@ -1,12 +1,36 @@
 package leetcode
 
 import (
-	"fmt"
 	"testing"
+
+	"github.com/stretchr/testify/require"
 )
 
 func TestNumRescueBoats(t *testing.T) {
-	//TODO: tc
-	res := numRescueBoats([]int{3, 2, 2, 1}, 3)
-	fmt.Printf("res: %v\n", res)
+	testCases := []struct {
+		peopleList []int // You are given an array people where people[i] is the weight of the i'th person
+		boatLimit  int
+		expected   int
+	}{
+		{
+			peopleList: []int{3, 2, 2, 1},
+			boatLimit:  3,
+			expected:   3,
+		},
+		{
+			peopleList: []int{3, 5, 3, 4},
+			boatLimit:  5,
+			expected:   4,
+		},
+		{
+			peopleList: []int{1, 2},
+			boatLimit:  3,
+			expected:   1,
+		},
+	}
+
+	for _, tc := range testCases {
+		res := numRescueBoats(tc.peopleList, tc.boatLimit)
+		require.Equal(t, tc.expected, res)
+	}
 }
