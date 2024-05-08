ac8c4c801e3ec3c637f99d1a01086c48e0ef47e4
Aleksey <aleksey.moiseenko79@gmail.com>
2024-05-08 19:26:42 +0300

    506. Relative Ranks test

--- leetcode/0506.RelativeRanks/506.RelativeRanks_test.go
+++ leetcode/0506.RelativeRanks/506.RelativeRanks_test.go
@@ -1,11 +1,28 @@
 package leetcode
 
 import (
-	"fmt"
 	"testing"
+
+	"github.com/stretchr/testify/require"
 )
 
 func TestFindRelativeRanks(t *testing.T) {
-	res := findRelativeRanks([]int{10, 3, 8, 9, 4})
-	fmt.Printf("res: %v\n", res)
+	testCases := []struct {
+		list     []int
+		expected []string
+	}{
+		{
+			list:     []int{10, 3, 8, 9, 4},
+			expected: []string{"Gold Medal", "5", "Bronze Medal", "Silver Medal", "4"},
+		},
+		{
+			list:     []int{5, 4, 3, 2, 1},
+			expected: []string{"Gold Medal", "Silver Medal", "Bronze Medal", "4", "5"},
+		},
+	}
+
+	for _, tc := range testCases {
+		res := findRelativeRanks(tc.list)
+		require.Equal(t, res, tc.expected)
+	}
 }
